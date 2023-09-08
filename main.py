8397bca85ccb356827d529f6984dbd4527f3ddab
nitin <nitin.jaitly@gmail.com>
2023-09-08 00:10:04 -0700

    tmp commit

--- main.py
+++ main.py
@@ -2,33 +2,34 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from rotate_array_by_one import *
+# from rotate_array_by_one import *
 # from is_happy_number import *
-from detect_cycle import *
-from find_m import *
+# from detect_cycle import *
+# from find_m import *
 # from test_print import *
 # from check_pair import *
 # from find_neighbor import *
-# from get_min_max import *
+from get_min_max import *
 # from reverse_list import *
 from array_sort import *
 
 
-def main():
-	# check_rotation()
-	# check_is_happy_number()
-	# check_detect_cycle()
-	# check_for_m_in_list()
-	# test_checkTwoSum()
-	# driver_find_peak()
+def main() -> object:
+    # check_rotation()
+    # check_is_happy_number()
+    # check_detect_cycle()
+    # check_for_m_in_list()
+    # test_checkTwoSum()
+    # driver_find_peak()
     # driver_min_max()
-	# drive_reverse_list()
-	selection_sort()
-    
+    # drive_reverse_list()
+    selection_sort()
+
+    return None
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-	main()
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
