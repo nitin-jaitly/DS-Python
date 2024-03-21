75c36828c9fc73f7bd420f6451006622c469f8f0
nitin-jaitly <nitin.jaitly@gmail.com>
2024-03-21 12:53:51 -0700

    check valid parenthesis using stack

---
+++ NeetCode_75/__04_Stack_01/valid_parenthesis.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def isValid(self, s:str) -> bool:
+        stack = []
+        closeToOpen = {")" : "(",
+                       "]" : "[",
+                       "}" : "{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+    def driver_valid_parenthesis(self):
+        s = "()"
+        if self.isValid(s):
+            print("PASS")
+
+
+        s = "()[]{}"
+        if self.isValid(s):
+            print("PASS")
+
+        s = "(]"
+        if self.isValid(s):
+            print("PASS")
+        else:
+            print("FAIL")
--- main.py
+++ main.py
@@ -1,60 +1,63 @@
-import NeetCode.__01__Arrays_and_Hashing_08.__01__contains_duplicate
-import NeetCode.__01__Arrays_and_Hashing_08.__02__valid_anagram
-import NeetCode.__01__Arrays_and_Hashing_08.__03__two_sum
-import NeetCode.__01__Arrays_and_Hashing_08.__04__group_anagrams
-import NeetCode.__01__Arrays_and_Hashing_08.__05__top_k_frequent
-import NeetCode.__01__Arrays_and_Hashing_08.__06__product_except_self
-import NeetCode.__01__Arrays_and_Hashing_08.__07_encode_decode_strings
-import NeetCode.__01__Arrays_and_Hashing_08.__08__longest_sequence
-import NeetCode.__01__Arrays_and_Hashing_08.abc
+import NeetCode_75.__01__Arrays_and_Hashing_08.__01__contains_duplicate
+import NeetCode_75.__01__Arrays_and_Hashing_08.__02__valid_anagram
+import NeetCode_75.__01__Arrays_and_Hashing_08.__03__two_sum
+import NeetCode_75.__01__Arrays_and_Hashing_08.__04__group_anagrams
+import NeetCode_75.__01__Arrays_and_Hashing_08.__05__top_k_frequent
+import NeetCode_75.__01__Arrays_and_Hashing_08.__06__product_except_self
+import NeetCode_75.__01__Arrays_and_Hashing_08.__07_encode_decode_strings
+import NeetCode_75.__01__Arrays_and_Hashing_08.__08__longest_sequence
+import NeetCode_75.__01__Arrays_and_Hashing_08.abc
 
-import NeetCode.__02__Two_pointers_03.__01__valid_palindrome
-import NeetCode.__02__Two_pointers_03.__02__three_sum
-import NeetCode.__02__Two_pointers_03.__03__container_with_most_water
+import NeetCode_75.__02__Two_pointers_03.__01__valid_palindrome
+import NeetCode_75.__02__Two_pointers_03.__02__three_sum
+import NeetCode_75.__02__Two_pointers_03.__03__container_with_most_water
 
-import NeetCode.__03__Sliding_window_04.__01_best_time_to_buy_sell_stock
-import NeetCode.__03__Sliding_window_04.__02_longest_substring_with_no_repeating_char
-import NeetCode.__03__Sliding_window_04.__03_longest_repeating_char_replacement
-import NeetCode.__03__Sliding_window_04.__04__minimum_window_substring
+import NeetCode_75.__03__Sliding_window_04.__01_best_time_to_buy_sell_stock
+import NeetCode_75.__03__Sliding_window_04.__02_longest_substring_with_no_repeating_char
+import NeetCode_75.__03__Sliding_window_04.__03_longest_repeating_char_replacement
+import NeetCode_75.__03__Sliding_window_04.__04__minimum_window_substring
 
-import Leetcode.Arrays_and_Hashing.remove_duplicates
-import Actual_Interview.find_primes
-
+import NeetCode_75.__04_Stack_01.valid_parenthesis
 
 def main() -> object:
+
+
+    # Sol = Actual_Interview.find_primes.Solution()
+    # Sol.driver_find_primes()
+
+    #
+    # Sol = Leetcode.Arrays_and_Hashing.remove_duplicates.Solution()
+    # Sol.driver_removeDuplicates()
 
     """
     __01__Arrays_and_Hashing_08
     """
 
-    Sol = Leetcode.Arrays_and_Hashing.remove_duplicates.Solution()
-    Sol.driver_removeDuplicates()
-
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__01__contains_duplicate.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__01__contains_duplicate.Solution()
     # Sol.driver_contains_duplicate()
 
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__02__valid_anagram.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__02__valid_anagram.Solution()
     # Sol.driver_is_anagram()
 
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__03__two_sum.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__03__two_sum.Solution()
     # Sol.driver_two_sum()
 
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__04__group_anagrams.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__04__group_anagrams.Solution()
     # Sol.driver_anagrams()
 
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__05__top_k_frequent.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__05__top_k_frequent.Solution()
     # Sol.driver_top_k_frequent()
 
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__06__product_except_self.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__06__product_except_self.Solution()
     # Sol.driver_product_except_self()
     #
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__07_encode_decode_strings.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__07_encode_decode_strings.Solution()
     # Sol.driver_encode_decode()
     #
-    # Sol = NeetCode.__01__Arrays_and_Hashing_08.__08__longest_sequence.Solution()
+    # Sol = NeetCode_75.__01__Arrays_and_Hashing_08.__08__longest_sequence.Solution()
     # Sol.driver_longest_seq()
-    # c1= NeetCode.__01__Arrays_and_Hashing_08.abc.cube(4)
-    # c2 = NeetCode.__01__Arrays_and_Hashing_08.abc.cube(8)
+    # c1= NeetCode_75.__01__Arrays_and_Hashing_08.abc.cube(4)
+    # c2 = NeetCode_75.__01__Arrays_and_Hashing_08.abc.cube(8)
     #
     #
     # c3 = c1 + c2
@@ -67,32 +70,33 @@ def main() -> object:
     """
     __02__Two_pointers_03
     """
-    # Sol = NeetCode.__02__Two_pointers_03.__01__valid_palindrome.Solution()
+    # Sol = NeetCode_75.__02__Two_pointers_03.__01__valid_palindrome.Solution()
     # Sol.driver_check_palindrome()
     #
-    # Sol = NeetCode.__02__Two_pointers_03.__02__three_sum.Solution()
+    # Sol = NeetCode_75.__02__Two_pointers_03.__02__three_sum.Solution()
     # Sol.driver_three_sum()
 
-    # Sol = NeetCode.__02__Two_pointers_03.__03__container_with_most_water.Solution()
+    # Sol = NeetCode_75.__02__Two_pointers_03.__03__container_with_most_water.Solution()
     # Sol.driver_max_water()
 
     """
     __03__Sliding_window_04 
     """
-    # Sol = NeetCode.__03__Sliding_window_04.__01_best_time_to_buy_sell_stock.Solution()
+    # Sol = NeetCode_75.__03__Sliding_window_04.__01_best_time_to_buy_sell_stock.Solution()
     # Sol.driver_max_profit()
 
-    # Sol = NeetCode.__03__Sliding_window_04.__02_longest_substring_with_no_repeating_char.Solution()
+    # Sol = NeetCode_75.__03__Sliding_window_04.__02_longest_substring_with_no_repeating_char.Solution()
     # Sol.driver_length_of_longest_substring()
     #
-    # Sol = NeetCode.__03__Sliding_window_04.03_longest_repeating_char_replacement.Solution()
+    # Sol = NeetCode_75.__03__Sliding_window_04.03_longest_repeating_char_replacement.Solution()
     # Sol.driver_check_max_count()
 
-    Sol = NeetCode.__03__Sliding_window_04.__04__minimum_window_substring.Solution()
-    Sol.driver_minWindow()
+    # Sol = NeetCode_75.__03__Sliding_window_04.__04__minimum_window_substring.Solution()
+    # Sol.driver_minWindow()
 
-    # Sol = Actual_Interview.find_primes.Solution()
-    # Sol.driver_find_primes()
+    Sol = NeetCode_75.__04_Stack_01.valid_parenthesis.Solution()
+    Sol.driver_valid_parenthesis()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
